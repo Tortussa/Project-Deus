2b752c5b20ed42bd1dcb25e25a394f62feeb0933
Tortussa <guidoparlatore11@gmail.com>
2022-04-14 17:05:13 +0200

    gravity

--- Progetto Dio/Assets/Scripts/PlayerController.cs
+++ Progetto Dio/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,23 @@ using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
-    private Player player;
     private int playerID = 0;
-    public Vector3 moveInput;
+    private Player player;
+
     [HideInInspector] public CharacterController cc;
+    
+    [Header("Movement")]
+    public Vector3 moveInput;
     public float speed;
+
+    [Header("Gravity")]
+    public float gravity = -9.81f;
+    public LayerMask floorMask;
+    public Vector3 groundSpherePosition;
+    public float groundSphereRadius;
+    public bool isGrounded;
+    private Vector3 velocity;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +32,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded=CheckGravity(); //Calls checksphere each frame, do it better
         GatherInput();
     }
 
@@ -34,6 +47,22 @@ public class PlayerController : MonoBehaviour
     }
     void Move()
     {
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
         cc.Move(moveInput * speed * Time.deltaTime);
+        cc.Move(velocity*Time.deltaTime);
+        if (isGrounded) velocity.y = -2;
+    }
+    bool CheckGravity()
+    {
+        if (Physics.CheckSphere(transform.position + groundSpherePosition, groundSphereRadius, floorMask)) return true;
+        return false;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position+groundSpherePosition, groundSphereRadius);
     }
 }
